69fe84d62f5d068d414501e762efa5861e0ef9cd
profe Tolo <profetolocka@gmail.com>
2024-12-24 19:05:22 -0300

    Nuevo archivo

--- 3.7_Global_refresh/3.7_Global_refresh.ino
+++ 3.7_Global_refresh/3.7_Global_refresh.ino
@@ -1,15 +1,23 @@
 #include <Arduino.h>
 #include "EPD.h"
 #include "EPD_GUI.h"
-#include "pic_scenario.h"
+#include "santa.h"
 uint8_t ImageBW[12480];
 void setup() {
   // put your setup code here, to run once:
+  
+  //Configura el pin 7 (xx) y activa el panel
   pinMode(7, OUTPUT);
   digitalWrite(7, HIGH);
 
+  //Iniciliza pines de control e interfaz
   EPD_GPIOInit();
+  
+  //Crea un buffer para la imagen
+  //Pasa la definicion de imagen, tamaño, rotacion 
   Paint_NewImage(ImageBW, EPD_W, EPD_H, Rotation, WHITE);
+  
+  //Limpia el buffer con blanco
   Paint_Clear(WHITE); //Clear Canvas
 
   EPD_FastInit();
@@ -19,7 +27,9 @@ void setup() {
   //---Power on interface
   //  EPD_PartInit();
   EPD_FastInit();
-  EPD_ShowPicture(0, 0, EPD_W, EPD_H, gImage_scenario_home, WHITE);
+  
+  //Transfiere imagen al panel
+  EPD_ShowPicture(0, 0, EPD_W, EPD_H, gImage_santa, WHITE);
   EPD_Display(ImageBW);
   EPD_Update();
   EPD_DeepSleep();
