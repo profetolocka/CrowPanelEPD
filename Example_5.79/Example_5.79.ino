c50df0a97d48d18cb1993426ccb4ed19db3fe012
profe Tolo <profetolocka@gmail.com>
2024-12-27 16:15:53 -0300

    Nueva imagen

--- Example_5.79/Example_5.79.ino
+++ Example_5.79/Example_5.79.ino
@@ -1,30 +1,50 @@
-#include "EPD.h"             // // Introduce EPD library for controlling electronic ink screens（E-Paper Display）
-#include "pic_home.h"        // Introduce header files containing image data
+#include "EPD.h"             // Funciones para controlar el panel
+#include "Example579.h"      // Imagen a cargar en el panel
 
-uint8_t ImageBW[27200];       // Declare an array with a size of 27200 bytes for storing black and white image data
+//OJO!! Debe ser si o si 27200, no 26928 que es el tamaño del .h
+//EPD_W=800, asi que 800x272 / 8 = 27200
+//En el video aclara que se define asi aunque el panel tenga menos
+uint8_t ImageBW[27200];      // Array para almacenar la informacion de color de la imagen
 
 void setup() {
-  // Initialize settings, executed once at program startup
-  pinMode(7, OUTPUT);        // Set pin 7 to output mode
-  digitalWrite(7, HIGH);     // Set pin 7 to high level to activate the screen power supply
+  
+  pinMode(7, OUTPUT);        
+  digitalWrite(7, HIGH);     // Activa la alimentacion del panel
 
-  EPD_GPIOInit();            // Initialize GPIO pin configuration for EPD e-ink screen
-  Paint_NewImage(ImageBW, EPD_W, EPD_H, Rotation, WHITE);  // Create a new image buffer with a size of EPD_W x EPD_H and a white background color
-  Paint_Clear(WHITE);       // Clear the image buffer and fill it with white
+  EPD_GPIOInit();            // Iniciliza pines de control e interfaz
+
+  //Inicializa un buffer para la imagen
+  //Pasa la definicion de imagen, tamaño, rotacion y color de fondo
+  Paint_NewImage(ImageBW, EPD_W, EPD_H, Rotation, WHITE);  
+  
+  //Se asegura de limpiar el buffer de imagen rellenando con color Blanco
+  Paint_Clear(WHITE);       
 
   /***********************Quick screen cleaning works in local flashing mode*************************/
+  //Inicializacion rápida
   EPD_FastMode1Init();       // Initialize EPD screen in Quick Mode 1
-  EPD_Display_Clear();        // Clear screen content
-  EPD_Update();             // Update screen display
+  
+  // Limpia la pantalla 
+  EPD_Display_Clear();      // Limpia la pantalla a nivel de hardware
+  EPD_Update();             // Refresca para que se vea
 
-  EPD_GPIOInit();            //Initialize the GPIO pin configuration of the EPD e-ink screen again
-  EPD_FastMode1Init();       //Re initialize the EPD screen in Quick Mode 1
-  //EPD_ShowPicture(0, 0, 792, 272, gImage_home, WHITE); // Display image gImage_1 with starting coordinates of (0,0), width of 792, height of 272, and a white background color
-  EPD_ShowPicture(0, 0, 792, 272, gImage_global_1, WHITE);
+  //Comente los 2 y funcionó igual
+  //EPD_GPIOInit();            //Initialize the GPIO pin configuration of the EPD e-ink screen again
+  //EPD_FastMode1Init();       //Re initialize the EPD screen in Quick Mode 1
+      //EPD_ShowPicture(0, 0, 792, 272, gImage_home, WHITE); // Display image gImage_1 with starting coordinates of (0,0), width of 792, height of 272, and a white background color
+  
+  // Pasa la imagen al buffer
+  //EPD_ShowPicture(0, 0, 792, 272, gImage_home, WHITE);
+
+  EPD_ShowPicture(0, 0, 792, 272, gImage_Example579, WHITE);
+  
+  // Transfiere la imagen a la pantalla
   EPD_Display(ImageBW);      // Display images stored in the ImageBW array
-  //  EPD_WhiteScreen_ALL_Fast(gImage_boot_setup); //Annotated code: Display boot settings image using quick mode
-  //  EPD_PartUpdate();       //Annotated code: Updating a part of the screen
-  EPD_FastUpdate();          //Perform quick updates to refresh the screen
+    //  EPD_WhiteScreen_ALL_Fast(gImage_boot_setup); //Annotated code: Display boot settings image using quick mode
+    //  EPD_PartUpdate();       //Annotated code: Updating a part of the screen
+  //EPD_FastUpdate();          //Perform quick updates to refresh the screen
+  
+  EPD_FastUpdate(); 
   EPD_DeepSleep();          //Set the screen to deep sleep mode to save power
   delay(5000);              // Wait for 5000 milliseconds (5 seconds)
 }
