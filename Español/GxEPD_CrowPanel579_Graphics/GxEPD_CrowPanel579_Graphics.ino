713d91527de6ac19ede03bef9f73ba668a04a701
profe Tolo <profetolocka@gmail.com>
2025-01-03 22:09:38 -0300

    Version funcional

--- Español/GxEPD_CrowPanel579_Graphics/GxEPD_CrowPanel579_Graphics.ino
+++ Español/GxEPD_CrowPanel579_Graphics/GxEPD_CrowPanel579_Graphics.ino
@@ -2,7 +2,7 @@
 #include <GxEPD2_BW.h>
 
 //Incluir las definiciones de las fuentes
-#include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 
 // Definición de pines para CrowPanel
 const int EINK_BUSY = 48;   
@@ -24,6 +24,15 @@ void displayPowerOn () {
 
 void setup() 
 {
+  
+  int canvasW = 500;
+  int canvasH = 200;
+  int canvasX = display.width()/2 - canvasW/2;
+  int canvasY = 50;
+
+  int values [50];
+
+  
   displayPowerOn ();
   
   // Inicialización del epaper
@@ -33,63 +42,55 @@ void setup()
   display.setRotation(0);
   
   display.setTextSize(1);
+
+  // Configuración del texto
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(&FreeMonoBold9pt7b); 
   
    // Limpiar la pantalla
   display.setFullWindow();
   display.fillScreen(GxEPD_WHITE); // Fondo blanco
-  // 1.Gráfico de tortas
-    
-  // Definir el centro y el radio del círculo
-  int centerX = 65;
-  int centerY = 80;
-  int radius = 40; 
-  // Dibujar el círculo relleno de rojo
-  display.fillCircle(centerX, centerY, radius, GxEPD_BLACK);
-  // Calcular los ángulos para las secciones del gráfico
-  float angle1 = 0;
-  float angle2 = 144; // 40% de 360 grados (0.4 * 360)
-  float angle3 = 252; // 144 + 108 (30% de 360 grados)
-  // Convertir grados a radianes
-  float angle1_rad = angle1 * PI / 180;
-  float angle2_rad = angle2 * PI / 180;
-  float angle3_rad = angle3 * PI / 180;
-  // Calcular las coordenadas de los puntos en el borde del círculo
-  int x1 = centerX + radius * cos(angle1_rad);
-  int y1 = centerY + radius * sin(angle1_rad);
-  int x2 = centerX + radius * cos(angle2_rad);
-  int y2 = centerY + radius * sin(angle2_rad);
-  int x3 = centerX + radius * cos(angle3_rad);
-  int y3 = centerY + radius * sin(angle3_rad);
-  // Dibujar las líneas desde el centro hasta el borde
-  display.drawLine(centerX, centerY, x1, y1, GxEPD_WHITE);
-  display.drawLine(centerX, centerY, x2, y2, GxEPD_WHITE);
-  display.drawLine(centerX, centerY, x3, y3, GxEPD_WHITE);
-  // 2.Gráfico de barras
-  int originX = 135;  //X del origen de coordenadas
-  int originY = 115;  //Y del origen de coordenadas
-  int barW = 20;      //Ancho de las barras
-  int barGap = 5;     //Separacion entre barras
-  int barH1 = 50;     //Altura barra1
-  int barH2 = 70;     //Altura barra2
-  int barH3 = 45;     //Altura barra3
-  // Dibujar los ejes X e Y
-  display.drawLine (originX, originY, originX+90, originY,    GxEPD_BLACK);
-  display.drawLine (originX, originY, originX,     originY-70, GxEPD_BLACK);
-  // Dibujar las barras
-  display.fillRect(originX+barGap,          originY-barH1, barW, barH1, GxEPD_BLACK);
-  display.fillRect(originX+2*barGap+barW,   originY-barH2, barW, barH2, GxEPD_BLACK);
-  display.fillRect(originX+3*barGap+2*barW, originY-barH3, barW, barH3, GxEPD_BLACK);
-  // 3.Texto del título
-    
-  // Configuración del texto
-  display.setTextColor(GxEPD_BLACK);
-  display.setCursor(15, 20);
-  display.setFont(&FreeMonoBold12pt7b); 
-  // Imprimir texto
-  display.print ("ACME Corporation");
+ 
+  //Canvas
+  display.drawRect(canvasX, canvasY, canvasW, canvasH, GxEPD_BLACK);
+
+  // Escala vertical
+  for (int x=1; x<50; x++) {
+    display.drawLine (canvasX+(10*x), canvasY, canvasX+(10*x), canvasY+canvasH, GxEPD_BLACK);
+  }
+
+  // Escala horizontal
+  for (int y=1; y<20; y++) {
+    display.drawLine (canvasX, canvasY+(10*y), canvasX+canvasW, canvasY+(10*y), GxEPD_BLACK);
+  }
+
+  // Valores verticales
+  display.setCursor(canvasX-70, canvasY);
+  display.print ("$100K");
+  display.setCursor(canvasX-70, canvasY+(canvasH/2));
+  display.print (" $50K");
+  display.setCursor(canvasX-70, canvasY+canvasH);
+  display.print ("  $0");
+
+  //Llenar la lista con valores aleatorios
+  for (int i=0; i<50; i++) {
+    values [i] = 70 + random(-20, 21);
+  }
+
+  // Factor de escala
+  int scale = canvasH / 100;
+
+  // Graficar los 50 valores
+  for (int i=0; i<50; i++) {
+    int y = values[i]*scale;
+    display.fillRect (canvasX+(i*10), canvasY + (canvasH -y), 10, y, GxEPD_BLACK);
+    Serial.println (y);
+  }
+
   // Refrescar pantalla
-  display.display ();
+  display.display (); 
 }
+
 void loop() {
   //No hace nada
 }
